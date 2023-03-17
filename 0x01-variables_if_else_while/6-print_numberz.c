73451640221cdfbc96306841d8fc8a545e3a09b8
nnoran <noranelsadig4@gmail.com>
2023-03-17 20:59:06 +0200

    m

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -9,11 +9,11 @@
 */
 int main(void)
 {
-	char num;
+	int num;
 
-	for (num = '0'; num <= '9'; num++)
+	for (num = 0; num <= 9; num++)
 	{
-		putchar(num);
+		putchar(num + 48);
 	}
 	putchar('\n');
 	return (0);
