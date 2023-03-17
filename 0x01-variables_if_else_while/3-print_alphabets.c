0b6451edbb54e39e12e441ba0dc54b270dda9046
nnoran <noranelsadig4@gmail.com>
2023-03-17 19:09:25 +0200

    m

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - printing alphabets
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int l , u;
+	for (l = 97; l <= 122; l++)
+	{
+		putchar(l);
+	} for (u = 65; u <= 90; u++)
+	{
+		putchar(u);
+	}
+putchar('\n');
+return (0);
+}
