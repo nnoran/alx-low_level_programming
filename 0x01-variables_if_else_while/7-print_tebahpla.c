a950e5524b929395d6fa84ff5c24f38caedcb8c4
nnoran <noranelsadig4@gmail.com>
2023-03-17 21:06:44 +0200

    m

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+*main - Entry point
+*
+*Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int n;
+
+	for (n = 122; n >= 97; n++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+	return (0);
+}
