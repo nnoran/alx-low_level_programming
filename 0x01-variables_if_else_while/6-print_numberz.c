eea470c80b7e3a7a048678636e9dc6803944503d
nnoran <noranelsadig4@gmail.com>
2023-03-17 20:52:43 +0200

    m

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+*main - numberz
+*
+*Return: Always 0 (Success)
+*/
+int main(void)
+{
+	char num;
+
+	for (num = '0'; num <= '9'; num++)
+	{
+		putchar(num);
+	}
+	putchar('\n');
+	return (0);
+}
