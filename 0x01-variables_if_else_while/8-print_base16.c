4dbe543b3fe39be17a1e5640f9eb4f98c41c7c64
nnoran <noranelsadig4@gmail.com>
2023-03-17 21:30:12 +0200

    m

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - hexadecimals
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+	int m;
+
+	for (n = 48; n <= 57; n++)
+	{
+		putchar(n);
+	}
+	for (m = 97; m <= 102; n++)
+	{
+		putchar(m);
+	}
+	putchar('\n');
+	return (0);
+}
