5ee4e4abe48278090b874a436451192c161f47bc
nnoran <noranelsadig4@gmail.com>
2023-03-22 10:57:32 +0200

    m

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet - lower case alphabet
+ *Return: Always 0 (Success)
+ */
+void print_alphabet(void)
+{
+        char letter;
+	int i;
+
+	for(i = 0; i <= 9; i++)
+	{
+        for (letter = 'a'; letter <= 'z'; letter++)
+                _putchar(letter);
+	}
+        _putchar('\n');
+}
