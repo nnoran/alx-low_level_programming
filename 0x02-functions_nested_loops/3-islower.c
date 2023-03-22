c239bceec9fa809a67f3a581becec12426801d27
nnoran <noranelsadig4@gmail.com>
2023-03-22 15:29:39 +0200

    m

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,16 +3,17 @@
 /**
  *  _islower - checks for lower case chars
  *  task 3
+ *  @c: char to be checked
  *  Return: 1 if char is lower case and 0 otherwise
  */
 int _islower(int c)
 {
-	char c;
+	int c;
 
 	c = getchar();
 		if (islower(c))
 		{
 			return (1);
-		} else 
+		} else
 			return (0);
 }
